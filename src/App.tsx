e3ac148ebeb222d783d1e1fcdb858f0d5e2441ff
Rody Huancas <rodyhuancas.04@gmail.com>
2025-10-04 17:31:17 -0500

    Feat: llamar componente de listado de productos

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import ListProducts from "./components/ListProducts";
+
+const App = () => {
+  return (
+    <section className="container mx-auto py-20">
+      <ListProducts />
+    </section>
+  );
+};
+
+export default App;
